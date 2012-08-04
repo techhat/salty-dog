2d5f91c60d705abf738012565e7494a07d2abdf3
Clint Savage <herlo1@gmail.com>
2012-08-04 13:30:48 -0600

    adding templating

--- dog.py
+++ dog.py
@@ -10,9 +10,20 @@ app.config.from_object(__name__)
 
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
+# the dataset below is just a representation of what
+# some function can do to get the values to pass along
+
+response = {
+        'menuitem': 'home',
+        'active_nodes':
+            [ 'node1', 'node2', 'node3', 'node4' ],
+        'inactive_nodes':
+            [ 'node5', 'node6', 'node7', 'node8' ],
+        }
+
 @app.route('/')
 def return_main():
-    return render_template('main.html')
+    return render_template('main.html', response=response)
 
 if __name__ == '__main__':
     app.run()
